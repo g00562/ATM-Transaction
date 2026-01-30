58959b79c8fe4c535146ee08137a9e741ce2b842
ishan <g00562215@gmail.com>
2026-01-30 10:55:09 +0530

    updated

--- atm_transact.py
+++ atm_transact.py
@@ -6,60 +6,66 @@ def balance(bal):
 def deposite():
     
     global bal
-    a = int(input("Enter amount to deposite:"))
+    a = float(input("\nEnter amount to deposite:"))
     
     if a > 0:
         bal += a
-        print(f"RS. {a} is deposited.")
+        print(f"\nRS. {a} is deposited.")
     
     else:
-        print("Invalid Input")
+        print("\nInvalid Input")
     return bal
 
 def withdraw():
     
-    global bal
-    a = int(input("Enter amount to withdraw:"))
+    global bal 
+
+    a = float(input("\nEnter amount to withdraw:"))
     
-    if 0 < a >= bal:
+    if 0 < a:
         
-        if a <= bal:
-            bal -= a
-            print(f"Rs. {a} is withdrawed.")
+        if a > bal:
+            print("\nYou don't have that much balance.")
+
         else:
-            print("You don't have that much balance.")
+            bal -= a
+            print(f"\nRs. {a} is withdrawed.")
+            
         
     else:
-        print("Envalid Input")
+        print("\nEnvalid Input")
     return bal
 
 def main():
+    
     while True:
+        
         print("\nEnter operation you want to perform.")
         print("1.View Balance")
         print("2.Deposite Money")
         print("3.Withdraw Money")
         print("4.Exit")
 
-        ch = int(input("Enter your choice:"))
+        ch = input("\nEnter your choice:")
 
-        if ch == 1:
+        if ch == "1":
+
             balance(bal)
         
-        elif ch == 2:
+        elif ch == "2":
             
             deposite()
         
-        elif ch ==3:
+        elif ch =="3":
             
             withdraw()
 
-        elif ch == 4:
-            print("Goodbye")
+        elif ch == "4":
+            print("\nGoodbye")
             break
         
         else:
-            print("Invalid input.")
+            print("\nInvalid input.")
 
 if __name__ == "__main__":
     main()
